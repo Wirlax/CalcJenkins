29886ecfda2ed6d81c89e2d363d48a60d7c8ee14
Ronan Thiébaut <ronan.thiebaut@outlook.fr>
2021-10-21 12:17:14 +0200

    Third Commit

--- src/calcjenkins/CalcJenkins.java
+++ src/calcjenkins/CalcJenkins.java
@@ -15,6 +15,36 @@ public class CalcJenkins {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int plus = plus(1,2);
+        int minus = minus(10,5);
+        int multiply = multiply(5,5);
+        int divideNotZero = divide(5,6);
+        //int divideZero = divide(5,0);
+        
+        
+        
+    }
+    
+    static public int plus(int number1, int number2){
+        return number1 + number2;
+    }
+    
+    static public int minus(int number1, int number2){
+        return number1 - number2;
+    }
+    
+    static public int multiply(int number1, int number2){
+        return number1 * number2;
+    }
+    
+    static public int divide(int number1, int number2){
+        
+       if(number2 != 0){
+           return number1 / number2;
+        } else {
+            throw new IllegalArgumentException("Argument 'divisor' must not be 0");
+        }
+        
     }
     
 }
--- test/calcjenkins/CalcJenkinsIT.java
+++ test/calcjenkins/CalcJenkinsIT.java
@@ -16,36 +16,58 @@ import static org.junit.Assert.*;
  * @author rthiebaut
  */
 public class CalcJenkinsIT {
-    
     public CalcJenkinsIT() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
-    /**
-     * Test of main method, of class CalcJenkins.
-     */
     @Test
-    public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        CalcJenkins.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testPlus() {
+        int number1 = 2;
+        int number2 = 3;
+        assertEquals(CalcJenkins.plus(number1,number2), 5);
     }
-    
+
+    @Test
+    public void testMinus() {
+        int number1 = 2;
+        int number2 = 3;
+        assertEquals(CalcJenkins.minus(number1,number2), -1);
+    }
+
+    @Test
+    public void testMultiply() {
+        int number1 = 2;
+        int number2 = 3;
+        assertEquals(CalcJenkins.multiply(number1,number2), 6);
+    }
+
+
+    @Test
+    public void testDivideNotZero() {
+        int number1 = 6;
+        int number2 = 3;
+        assertEquals(CalcJenkins.divide(number1,number2), 2);
+    }
+
+    @Test
+    public void testDivideZero() {
+        int number1 = 6;
+        int number2 = 0;
+    }
+
 }
